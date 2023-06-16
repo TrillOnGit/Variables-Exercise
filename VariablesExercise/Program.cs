6474cbd05851e1569d261736625b86b4af6d76ee
TrillOnGit <austinfunk99@yahoo.ca>
2023-06-16 10:46:38 -0400

    Defined and initialized variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
+            string catName = "Bill";
+            int catWeightIbs = 42;
+            char catFirstInitial = 'B';
+            bool isOverweight = true;
+            double preciseWeight = 41.8234567890123;
+            decimal yearsInVet = 0.000762M;
+
             Console.WriteLine("Hello, World!");
+            
         }
     }
 }
